2e05bc8e6ccdca5b7a3c1787cac37b351ba13822
ski429 <s.kim524@gmail.com>
2021-03-19 23:11:20 -0500

    attach database to pull menu items in order to render homepage

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,9 @@
-from flask import render_template, url_for
+from flask import render_template, redirect, request, jsonify
 from app import app
-from app.models import Item, Order, ItemsByOrder
-
-
-menu = [
-    {
-        'name': 'sausage pizza',
-        'pic': 1,
-        'cost': 10
-    },
-    {
-        'name': 'pepperoni pizza',
-        'pic': 2,
-        'cost': 10
-    },
-    {
-        'name': 'cheese pizza',
-        'pic': 3,
-        'cost': 7
-    }
-]
+from app.models import Item
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', menu=menu)
+    query = Item.query.all()
+    return render_template('index.html', menu=query)
