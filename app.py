d9a69a85d91776ae1c480de7822a8a63b37772a7
ski429 <s.kim524@gmail.com>
2021-03-19 21:15:13 -0500

    Implement databse schema

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, url_for
+import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(20), unique=True)
+    item_cost = db.Column(db.Float)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"Item('{self.item_name}', '{self.id}')"
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_time = db.Column(db.DateTime, nullable=False)
+    order_total = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Item('{self.order_total}', '{self.id}')"
+
+
+class ItemsByOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qty = db.Column(db.Integer, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+
 
 menu = [
     {
