be692817190c3d7caab355a32521a732d8f08fa2
ski429 <s.kim524@gmail.com>
2021-03-21 01:04:51 -0500

    refactor html: add base.html template

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ def index():
         db.session.commit()
         new_order_id = new_order.id
         for item in menu_query:
-            quantity = request.form[f'item{item.id}']
+            quantity = request.form[f"item{item.id}"]
             new_item_by_order = ItemsByOrder(qty=quantity, item_id=item.id, order_id=new_order_id)
             db.session.add(new_item_by_order)
         db.session.commit()
@@ -30,20 +30,22 @@ def order_detail(order_num):
     total = 0
     # Multiple table join to get complete order information
     order_placed = db.session.query(Order, ItemsByOrder, Item)\
-          .join(ItemsByOrder, ItemsByOrder.order_id == Order.id)\
-          .join(Item, Item.id == ItemsByOrder.item_id).filter(Order.id==order_num)
+        .join(ItemsByOrder, ItemsByOrder.order_id == Order.id)\
+        .join(Item, Item.id == ItemsByOrder.item_id).filter(Order.id == order_num)
     # Retrieve ItemsByOrder.qty and Item.item_cost to calc total cost
     for item in order_placed:
         if item[1].qty and item[1].qty > 0:
             total += item[1].qty * item[2].item_cost
     # Update missing Order.order_total
+    order_to_update = Order.query.filter_by(id=order_num).first()
+    order_to_update.order_total = total
     db.session.commit()
     return render_template('your_order_details.html', data=order_placed)
 
 
-@app.route('/order_cancel/<int:order_num>', methods=['DELETE'])
+@app.route('/order_cancel/<int:order_num>', methods=['GET', 'DELETE'])
 def delete_order(order_num):
-    order_to_delete = Order.query.filter_by(order_id=order_num)
+    order_to_delete = Order.query.filter_by(id=order_num).first()
     db.session.delete(order_to_delete)
     db.session.commit()
     return render_template('order_canceled.html', order_id=order_num)
